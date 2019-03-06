05a7c06f7dcab254d006c13bb7daed3a12e918ac
splendid-frame-7429 <bsameera@yahoo.com>
2019-03-06 20:06:11 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,11 +2,12 @@ def alphabetize(arr)
   # code here
   new_arr = arr.map { |word|
     word.include?('ĉ') ? word.gsub!('ĉ', 'cx') : word
-    word.include?('ĉ') ? word.gsub!('ĉ', 'cx') : word
+    word.include?('ĝ') ? word.gsub!('ĝ', 'gx') : word
   } 
   result = new_arr.sort
   final_result = result.map { |word|
     word.include?('cx') ? word.gsub!('cx', 'ĉ') : word
+    word.include?('gx') ? word.gsub!('gx', 'ĝ') : word
   }
   final_result
 end
