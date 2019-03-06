4d685d70a16cd3874edb3056c98ee4d73e27c694
splendid-frame-7429 <bsameera@yahoo.com>
2019-03-06 19:56:42 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,8 +2,8 @@ def alphabetize(arr)
   # code here
   new_arr = arr.map do |word|
     #if word.include?('ĉ')
-      word = word.gsub!('ĉ', 'cx')
-      word
+      word.gsub!('ĉ', 'cx')
+  
     #end 
   end 
   result = new_arr.sort
